38042e19451cd842753fb6ba55ffe2acdf6f5508
Saimon <mehedy303@gmail.com>
2026-03-14 03:06:17 +0600

    testing it

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: ["103.6.169.235", "http://103.6.169.235", "https://103.6.169.235"],
-  },
 };
 
 export default nextConfig;
